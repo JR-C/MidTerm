f9fec961779b864d6a3fb92b06942f080d19da52
Jardiel Ramos <jeramos@student.fullsail.edu>
2020-08-08 17:36:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# all_in_map
+ All In Map Flutter Application
---
+++ android/app/src/main/java/jardielramos/allinmap/MainActivity.java
@@ -0,0 +1,6 @@
+package jardielramos.allinmap;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
