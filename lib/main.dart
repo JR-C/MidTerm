c2b610cf9aa236071cc80e31da5aa4c2432d3f1d
Jardiel Ramos <jeramos@student.fullsail.edu>
2020-08-10 14:18:16 -0400

    Set up hot reload.
    Set application with hot relead in a stateless widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
 //Starting point of app
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.greenAccent[200],
-          appBar: AppBar(
-            title: Text('All In Map'),
-            backgroundColor: Colors.green[900],
-          ),
-          body: Center(
-            child: Image(
-              image: NetworkImage(
-                  'https://www.rodenstock.com/mediadownload/_images/_live_b2c/medias/0000/00/1d/58/1923327/Rodenstock-Auto-0001-1360x680px-020-min.jpg'),
-            ),
+void main() => runApp(AllInMap());
+
+class AllInMap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent[200],
+        appBar: AppBar(
+          title: Text('All In Map'),
+          backgroundColor: Colors.green[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://www.rodenstock.com/mediadownload/_images/_live_b2c/medias/0000/00/1d/58/1923327/Rodenstock-Auto-0001-1360x680px-020-min.jpg'),
           ),
         ),
       ),
     );
+  }
+}
