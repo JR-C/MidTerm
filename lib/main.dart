899f804f7685deb5adfa86dab5339b41c08653df
Jardiel Ramos <jeramos@student.fullsail.edu>
2020-08-23 20:11:56 -0400

    Changed Design
    Wrapped the row widget in a column widget to allow it to be placed into at the bottom of the screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,66 +8,71 @@ class AllInMap extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey[500],
+        backgroundColor: Colors.grey[300],
         appBar: AppBar(
           title: Text('All In Map'),
-          backgroundColor: Colors.green[900],
+          backgroundColor: Colors.teal[900],
         ),
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.end,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              Container(
-                height: double.infinity,
-              ),
-              Container(
-                color: Colors.green[900],
-                width: 100.0,
-                height: 60.0,
-                child: Center(
-                  child: Text(
-                    'Map',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      color: Colors.teal[900],
+                      width: 125.0,
+                      height: 60.0,
+                      child: Center(
+                        child: Text(
+                          'Map',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Container(
-                color: Colors.green[900],
-                width: 100.0,
-                height: 60.0,
-                child: Center(
-                  child: Text(
-                    'Weather',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
+                  Expanded(
+                    child: Container(
+                      color: Colors.teal[900],
+                      width: 125.0,
+                      height: 60.0,
+                      child: Center(
+                        child: Text(
+                          'Weather',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Container(
-                color: Colors.green[900],
-                width: 100.0,
-                height: 60.0,
-                child: Center(
-                  child: Text(
-                    'Time',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
+                  Expanded(
+                    child: Container(
+                      color: Colors.teal[900],
+                      width: 125.0,
+                      height: 60.0,
+                      child: Center(
+                        child: Text(
+                          'Time',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Container(
-                height: double.infinity,
+                ],
               ),
             ],
           ),
