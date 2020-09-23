3990bf7e8e0aff537cea7e789696765f48863fd1
Jardiel Ramos <jeramos@student.fullsail.edu>
2020-09-23 16:38:30 -0400

    Moved UI code to Stateful Widget
    Created Statefull Widget and add the User interface into it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,173 @@
 import 'package:allinmap/gmap.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 //Starting point of app
 void main() => runApp(AllInMap());
 
-class MapsDemo extends StatefulWidget {
+class AllInMap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'All In Map',
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          title: Text('All In Map'),
+          backgroundColor: Colors.teal[900],
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => GMap(),
+                          ),
+                        );
+                        print("pressed");
+                      },
+                      padding: EdgeInsets.all(1.0),
+                      child: Container(
+                        color: Colors.teal[900],
+                        width: 135.0,
+                        height: 60.0,
+                        child: Center(
+                          child: Text(
+                            'Map',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: FlatButton(
+                      onPressed: () {
+                        print('Clicked Weather Tab');
+                      },
+                      padding: EdgeInsets.all(1.0),
+                      child: Container(
+                        color: Colors.teal[900],
+                        width: 135.0,
+                        height: 60.0,
+                        child: Center(
+                          child: Text(
+                            'Weather',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: FlatButton(
+                      onPressed: () {
+                        print('Clicked Time Tab');
+                      },
+                      padding: EdgeInsets.all(1.0),
+                      child: Container(
+                        color: Colors.teal[900],
+                        width: 135.0,
+                        height: 60.0,
+                        child: Center(
+                          child: Text(
+                            'Time',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}*/
+/*class MapsDemo extends StatefulWidget {
   MapsDemo() : super();
   @override
   _MapsDemoState createState() => _MapsDemoState();
@@ -101,108 +262,10 @@ class _MapsDemoState extends State<MapsDemo> {
       ),
     );
   }
-}
+}*/
 
-class AllInMap extends StatelessWidget {
+/*class AllInMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey[300],
-        appBar: AppBar(
-          title: Text('All In Map'),
-          backgroundColor: Colors.teal[900],
-        ),
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Expanded(
-                    child: FlatButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => GMap(),
-                          ),
-                        );
-                        print("pressed");
-                        MapsDemo();
-                      },
-                      padding: EdgeInsets.all(1.0),
-                      child: Container(
-                        color: Colors.teal[900],
-                        width: 135.0,
-                        height: 60.0,
-                        child: Center(
-                          child: Text(
-                            'Map',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: FlatButton(
-                      onPressed: () {
-                        print('Clicked Weather Tab');
-                      },
-                      padding: EdgeInsets.all(1.0),
-                      child: Container(
-                        color: Colors.teal[900],
-                        width: 135.0,
-                        height: 60.0,
-                        child: Center(
-                          child: Text(
-                            'Weather',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: FlatButton(
-                      onPressed: () {
-                        print('Clicked Time Tab');
-                      },
-                      padding: EdgeInsets.all(1.0),
-                      child: Container(
-                        color: Colors.teal[900],
-                        width: 135.0,
-                        height: 60.0,
-                        child: Center(
-                          child: Text(
-                            'Time',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
   }
-}
+}*/
