39785dd6d74b8fa19cf22067dc54b6349d713bd0
Jardiel Ramos <jeramos@student.fullsail.edu>
2020-09-30 13:26:26 -0400

    Weather button calls OpenWeather
    Add a TEST text to OpenWeather.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:allinmap/gmap.dart';
+import 'package:allinmap/oweather.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -76,7 +77,12 @@ class _MainScreenState extends State<MainScreen> {
                   Expanded(
                     child: FlatButton(
                       onPressed: () {
-                        print('Clicked Weather Tab');
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => OpenWeather(),
+                          ),
+                        );
                       },
                       padding: EdgeInsets.all(1.0),
                       child: Container(
@@ -128,142 +134,3 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 }
-
-/*class MapSample extends StatefulWidget {
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
-    );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
-}*/
-/*class MapsDemo extends StatefulWidget {
-  MapsDemo() : super();
-  @override
-  _MapsDemoState createState() => _MapsDemoState();
-}
-
-class _MapsDemoState extends State<MapsDemo> {
-  Completer<GoogleMapController> _controller = Completer();
-  static const LatLng _center = const LatLng(28.5966, 81.3013);
-  final Set<Marker> _markers = {};
-  LatLng _lastMapPosition = _center;
-  MapType _currentMapType = MapType.normal;
-
-  _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
-
-  _onCameraMove(CameraPosition position) {
-    _lastMapPosition = position.target;
-  }
-
-  _onMapTypeButtonPressed() {
-    setState(() {
-      _currentMapType = _currentMapType == MapType.normal
-          ? MapType.satellite
-          : MapType.normal;
-    });
-  }
-
-  _onAddMarkerButtonPressed() {
-    setState(() {
-      _markers.add(
-        Marker(
-            markerId: MarkerId(_lastMapPosition.toString()),
-            position: _lastMapPosition,
-            infoWindow: InfoWindow(
-              title: 'Title',
-              snippet: 'Snip',
-            )),
-      );
-    });
-  }
-
-  Widget button(Function function, IconData icon) {
-    return FloatingActionButton(
-      onPressed: function,
-      materialTapTargetSize: MaterialTapTargetSize.padded,
-      backgroundColor: Colors.teal[900],
-      child: Icon(
-        icon,
-        size: 36.0,
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('All In Map'),
-          backgroundColor: Colors.teal[900],
-        ),
-        body: Stack(
-          children: <Widget>[
-            GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 11.0,
-              ),
-              mapType: _currentMapType,
-              markers: _markers,
-              onCameraMove: _onCameraMove,
-            ),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Align(
-                alignment: Alignment.topRight,
-                child: Column(
-                  children: <Widget>[
-                    button(_onMapTypeButtonPressed, Icons.map),
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    button(_onAddMarkerButtonPressed, Icons.add_location),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}*/
--- lib/oweather.dart
+++ lib/oweather.dart
@@ -1,4 +1,16 @@
 import 'dart:collection';
-
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:weather/weather.dart';
+
+class OpenWeather extends StatefulWidget {
+  @override
+  _OpenWeatherState createState() => _OpenWeatherState();
+}
+
+class _OpenWeatherState extends State<OpenWeather> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('TEST');
+  }
+}
