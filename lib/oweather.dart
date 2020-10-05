990fb7c869a4465e56b6e54d87f0df188b62196d
Jardiel Ramos <jeramos@student.fullsail.edu>
2020-10-05 17:30:01 -0400

    Add Sample Temp Text
    Added temperature int and set it in center of image, inside a column.

--- lib/oweather.dart
+++ lib/oweather.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weather/weather.dart';
 
@@ -9,12 +10,31 @@ class OpenWeather extends StatefulWidget {
 }
 
 class _OpenWeatherState extends State<OpenWeather> {
+  int temperature = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('images/clear.png'),
+          image: AssetImage(
+            'images/clear.png',
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Text(
+                temperature.toString() + '\u2109',
+              ),
+            ),
+          ],
         ),
       ),
     );
