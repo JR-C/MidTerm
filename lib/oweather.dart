2714977db44b60286edd019685f8000dcf0faca7
Jardiel Ramos <jeramos@student.fullsail.edu>
2020-10-12 13:45:37 -0400

    Added conversion
    Added conversion formula from Celsius to fahrenheit

--- lib/oweather.dart
+++ lib/oweather.dart
@@ -1,8 +1,5 @@
-import 'dart:collection';
-import 'dart:async';
-import 'package:flutter/cupertino.dart';
+//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:weather/weather.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -13,33 +10,33 @@ class OpenWeather extends StatefulWidget {
 
 class _OpenWeatherState extends State<OpenWeather> {
   int temperature = 0;
-  String location = 'Orlando';
-  int woeid = 2466256;
+  String location = 'Miami';
+  int woeid = 2450022;
   String weather = 'clear';
 
   String searchApiUrl =
       'https://www.metaweather.com/api/location/search/?query=';
   String locationApiUrl = 'https://www.metaweather.com/api/location/';
 
-  void fetchSearch(String search) async {
-    var searchResult = await http.get(searchApiUrl + search);
+  void fetchSearch(String input) async {
+    var searchResult = await http.get(searchApiUrl + input);
     var result = json.decode(searchResult.body)[0];
 
     setState(() {
-      location = result['title'];
-      woeid = result['woeid'];
+      location = result["title"];
+      woeid = result["woeid"];
     });
   }
 
   void fetchLocation() async {
     var locationResult = await http.get(locationApiUrl + woeid.toString());
     var result = json.decode(locationResult.body);
-    var consolidated_weather = result['consolidate_weather'];
-    var data = consolidated_weather[0];
+    var consolidatedWeather = result["consolidated_weather"];
+    var data = consolidatedWeather[0];
 
     setState(() {
-      temperature = data['the_temp'].round();
-      weather = data['weather_state_name'].replaceAll(' ', '').toLowerCase();
+      temperature = data["the_temp"].round();
+      weather = data["weather_state_name"].replaceAll(' ', '').toLowerCase();
     });
   }
 
@@ -50,41 +47,42 @@ class _OpenWeatherState extends State<OpenWeather> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(
-            'images/$weather.png',
+    return MaterialApp(
+      home: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'images/$weather.png',
+            ),
+            fit: BoxFit.cover,
           ),
-          fit: BoxFit.cover,
         ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Center(
-                  child: Text(
-                    temperature.toString() + '\u2109',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 50.0,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Center(
+                    child: Text(
+                      (temperature * 9 / 5 + 32).round().toString() + '\u2109',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50.0,
+                      ),
                     ),
                   ),
-                ),
-                Text(
-                  location,
-                  style: TextStyle(color: Colors.white, fontSize: 40.0),
-                ),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Container(
+                  Text(
+                    location,
+                    style: TextStyle(color: Colors.white, fontSize: 40.0),
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
                     width: 300,
                     child: TextField(
                       onSubmitted: (String input) {
@@ -92,19 +90,22 @@ class _OpenWeatherState extends State<OpenWeather> {
                       },
                       style: TextStyle(color: Colors.white, fontSize: 25),
                       decoration: InputDecoration(
-                          hintText: 'Search Location...',
-                          hintStyle: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.0,
-                          ),
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.white,
-                          )),
-                    ))
-              ],
-            ),
-          ],
+                        hintText: 'Search Location...',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
